c98f17e0ab40064b0b077ec1101e4f380ec09b78
manuel-Igtm <njoguimmanuel8@gmail.com>
2025-10-07 11:08:25 +0300

    adding location such as country and so into the models.py

--- backend_security/ip_tracking/models.py
+++ backend_security/ip_tracking/models.py
@@ -6,6 +6,7 @@ class RequestLog(models.Model):
     path = models.CharField(max_length=255)
     country = models.CharField(max_length=100, blank=True, null=True)
     city = models.CharField(max_length=100, blank=True, null=True)
+    
     def __str__(self):
         return f"{self.ip_address} - {self.path}"
 
